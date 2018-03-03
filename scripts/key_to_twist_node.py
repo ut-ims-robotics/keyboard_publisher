4199b976e00e737ef8bcc379bd2f6a5b7f46ac6b
Martin Appo <martinappo@gmail.com>
2018-03-03 18:33:51 +0200

    Fix publishing

--- scripts/key_to_twist_node.py
+++ scripts/key_to_twist_node.py
@@ -30,27 +30,31 @@ class KeyToTwist(KeyListener):
 
         if key_event_msg.char == "w":  # forward
             if pressed:
-                self.twist_msg.twist.linear.x = 0.3
+                self.twist_msg.twist.linear.x = 0.2
+            self.twist_pub.publish(self.twist_msg)
 
         if key_event_msg.char == "s":  # backward
             if pressed:
-                self.twist_msg.twist.linear.x = -0.3
+                self.twist_msg.twist.linear.x = -0.2
+            self.twist_pub.publish(self.twist_msg)
 
         if key_event_msg.char == "d":
             if pressed:
-                self.twist_msg.twist.linear.y = 0.3
+                self.twist_msg.twist.linear.y = 0.2
+            self.twist_pub.publish(self.twist_msg)
 
         if key_event_msg.char == "a":
             if pressed:
-                self.twist_msg.twist.linear.y = -0.3
+                self.twist_msg.twist.linear.y = -0.2
+            self.twist_pub.publish(self.twist_msg)
 
-        self.twist_pub.publish(self.twist_msg)
+
 
 
 if __name__ == '__main__':
     try:
         rospy.init_node("key_to_twist_node")
-        rate = rospy.Rate(30)
+        rate = rospy.Rate(100)
         key_publisher = KeyToTwist()
 
         while not rospy.is_shutdown():
